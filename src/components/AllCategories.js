fd66e79c017653f4e8ff126664aae015acc78d00
PTC-TCOM02\divine.sackitey <divine.sackitey@petraonline.com>
2023-06-10 22:56:19 +0000

    styling for products page

--- src/components/AllCategories.js
+++ src/components/AllCategories.js
@@ -1,17 +1,50 @@
+import React, { useEffect, useState } from 'react'
 import axios from 'axios'
-import React from 'react'
+import '../components/Allcategories.css'
 
 const AllCategories = () => {
-    const videoList = () => {
-        axios.get('https://ec2-44-197-193-3.compute-1.amazonaws.com/api/User/GetAllProducts').then((res)=>{
-            console.log(res)
-        }).catch((err) => {
-            console.log(err)
-        })
+
+  useEffect(()=>{
+    getData();
+  }, [])
+
+  const [apiData, setApiData] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  const getData = async () => {
+    try {
+    const res = await axios.get('https://fakestoreapi.com/products');
+    setApiData(res.data);
+    setLoading(true);
+    } catch (err) {
+      alert(err.message);
     }
+  }
+
   return (
-    <div className='allcategories'>
-        <button onClick={videoList} className='getdatabtn'>Get Data</button>
+    <div className='productscontainer'>
+        <div className='productspagetitle'>
+            <h1><strong>ALL PRODUCTS</strong></h1>
+        </div>
+        <div className='productssection'>
+        {loading && 
+      apiData.map((dat) => 
+       (
+          <div className='productcontainer'>
+            <div>
+              <img src={dat.image} className='img' />
+              <div className='namesprice'>
+                {dat.title}<br/>
+                <strong>GHC {dat.price.toFixed(2)}</strong>
+              </div>
+              <div>
+                <button>Add to Cart</button>
+              </div>
+            </div>
+          </div>   
+        )
+      )}
+        </div>  
     </div>
   )
 }
