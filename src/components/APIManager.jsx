d94fe0f50e60001ffc41c02d0bfbedf427218bfb
KobinaY <134865586+KobinaY@users.noreply.github.com>
2023-08-06 13:36:34 -0400

    APIManager updated with credential set to "TRUE"

--- src/components/APIManager.jsx
+++ src/components/APIManager.jsx
@@ -3,7 +3,7 @@ import axios from "axios";
 const APIManager = axios.create({
     baseURL:'https://ec2-44-197-193-3.compute-1.amazonaws.com/api/',
     responseType:'json',
-    withCredentials:false,
+    withCredentials:true,
 })
 
 export default APIManager;
