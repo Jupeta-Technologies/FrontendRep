192c27531beddb7b8450adb36c78acf970ccc7cc
PTC-TCOM02\divine.sackitey <divine.sackitey@petraonline.com>
2023-08-11 23:55:43 +0000

    rebuild tile

---
+++ src/components/missylay.js
@@ -0,0 +1,32 @@
+<Container style={{width: '100vw'}}>
+      <Row>
+        <Col style={{ height: '400px', display: 'flex', justifyContent: 'center'}} xs={5}>
+          <section style={{width: '100%', height: '100%', padding: '10px'}}>
+            <img src={fben} style={{width: '100%', height: '100%', objectFit: 'fill', borderRadius: '7px'}} alt='' />
+          </section>
+        </Col>
+        <Col style={{ height: '400px', display: 'flex', justifyContent: 'center'}} xs={2}>
+          <section style={{width: '100%', height: '100%', padding: '10px'}}>
+            <img src={j} style={{width: '100%', height: '100%', objectFit: 'fill', borderRadius: '7px'}} alt='' />
+          </section>
+        </Col>
+        <Col style={{height: '400px', display: 'flex', flexDirection: 'column'}} xs={5}>
+          <section style={{height: '50%', display: 'flex', gap: '5px', marginBottom: '5px'}}>
+            <div style={{width: '50%', backgroundColor: 'pink'}}>
+              <img src={beats} alt='' style={{width: '100%', height: '100%', objectFit: 'fill', borderRadius: '7px'}}/>
+            </div>
+            <div style={{width: '50%', backgroundColor: 'whitesmoke'}}>
+              <img src={beats} alt='' style={{width: '100%', height: '100%', objectFit: 'fill', borderRadius: '7px'}}/>
+            </div>
+          </section>
+          <section style={{height: '50%',backgroundColor: 'orange', display: 'flex', gap: '5px'}}>
+            <div style={{width: '50%', backgroundColor: 'blue'}}>
+              <img src={beats} alt='' style={{width: '100%', height: '100%', objectFit: 'fill', borderRadius: '7px'}}/>
+            </div>
+            <div style={{width: '50%', backgroundColor: 'brown'}}>
+              <img src={beats} alt='' style={{width: '100%', height: '100%', objectFit: 'fill', borderRadius: '7px'}}/>
+            </div>
+          </section>
+        </Col>
+      </Row>
+    </Container>
