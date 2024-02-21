4282f319cb717b7c21bd9517ab9db2c56dd3c6ee
Christian Amoanyi <ChristianA@Chris-MacBook-Pro.local>
2024-02-21 17:16:39 +0000

    payment section progress

--- src/index.js
+++ src/index.js
@@ -6,13 +6,16 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {BrowserRouter} from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Context } from "./context/context";
 
 
 const root = createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
+    <Context>
       <App />
+      </Context>
     </BrowserRouter>
   </React.StrictMode>
 );
